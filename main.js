6e3a340f8d6bbbb254aa45002da19366fe3cae0c
ELVIS NASCIMENTO <elvisnascimentodev@gmail.com>
2023-07-23 13:02:16 -0300

    quarta questao implementada

--- main.js
+++ main.js
@@ -1,78 +1,96 @@
 let questaoAtual = 1;
 const totalQuestoes = 12;
 
-  function exibirQuestao(numeroQuestao) {
-    const questoes = document.querySelectorAll('.questao');
-    questoes.forEach(questao => questao.classList.remove('ativa'));
-    document.getElementById(`questao${numeroQuestao}`).classList.add('ativa');
-    
-    questoes.forEach(questao => questao.style.display = questao.classList.contains('ativa') ? 'block' : 'none');
-    }
+function exibirQuestao(numeroQuestao) {
+  const questoes = document.querySelectorAll('.questao');
+  questoes.forEach(questao => questao.classList.remove('ativa'));
+  document.getElementById(`questao${numeroQuestao}`).classList.add('ativa');
 
-  function proximaQuestao() {
-    if (questaoAtual < totalQuestoes) {
-      questaoAtual++;
-      exibirQuestao(questaoAtual);
-    }
+  questoes.forEach(questao => questao.style.display = questao.classList.contains('ativa') ? 'block' : 'none');
+}
+
+function proximaQuestao() {
+  if (questaoAtual < totalQuestoes) {
+    questaoAtual++;
+    exibirQuestao(questaoAtual);
   }
+}
 
-  function anteriorQuestao() {
-    if (questaoAtual > 1) {
-      questaoAtual--;
-      exibirQuestao(questaoAtual);
-    }
+function anteriorQuestao() {
+  if (questaoAtual > 1) {
+    questaoAtual--;
+    exibirQuestao(questaoAtual);
   }
-  // Exibir a primeira questão inicialmente
-  exibirQuestao(questaoAtual);
+}
+// Exibir a primeira questão inicialmente
+exibirQuestao(questaoAtual);
 
-  function parOuImpar(idInput, idResultado) {
-    const numero = parseInt(document.getElementById(idInput).value);
-    const resultadoElement = document.getElementById(idResultado);
+//resolucao da 1° pergunta
+function parOuImpar(idInput, idResultado) {
+  const numero = parseInt(document.getElementById(idInput).value);
+  const resultadoElement = document.getElementById(idResultado);
 
-    if (!isNaN(numero)) {
-      if (numero % 2 === 0) {
-        resultadoElement.textContent = "O número é par.";
-      } else {
-        resultadoElement.textContent = "O número é ímpar.";
-      }
+  if (!isNaN(numero)) {
+    if (numero % 2 === 0) {
+      resultadoElement.textContent = "O número é par.";
     } else {
-      resultadoElement.textContent = "Digite um número válido.";
+      resultadoElement.textContent = "O número é ímpar.";
     }
+  } else {
+    resultadoElement.textContent = "Digite um número válido.";
   }
-  function numeroMaior(idInput1,idInput2, idResultado) {
-    const numero1 = parseInt(document.getElementById(idInput1).value);
-    const numero2= parseInt(document.getElementById(idInput2).value);
-    const resultadoElement = document.getElementById(idResultado);
-    if (!isNaN(numero1) && !isNaN(numero2)) {
-      if (numero1 === numero2) {
-        resultadoElement.textContent = 'Os números são iguais!';
-      } else {
-        resultadoElement.textContent = numero1 > numero2 ? 'O 1° número é maior' : 'O 2° número é maior';
-      }
+}
+
+//resolucao da 2° pergunta
+
+function numeroMaior(idInput1, idInput2, idResultado) {
+  const numero1 = parseInt(document.getElementById(idInput1).value);
+  const numero2 = parseInt(document.getElementById(idInput2).value);
+  const resultadoElement = document.getElementById(idResultado);
+  if (!isNaN(numero1) && !isNaN(numero2)) {
+    if (numero1 === numero2) {
+      resultadoElement.textContent = 'Os números são iguais!';
     } else {
-      resultadoElement.textContent = 'Digite números válidos.';
+      resultadoElement.textContent = numero1 > numero2 ? 'O 1° número é maior' : 'O 2° número é maior';
     }
-    
+  } else {
+    resultadoElement.textContent = 'Digite números válidos.';
   }
+}
 
+// resolucao da 3° pergunta
 
+function votaOuNaoVota(idInput1, idResultado) {
+  let ano_Nascimento = parseInt(document.getElementById(idInput1).value);
+  const resultadoElement = document.getElementById(idResultado);
+  let ano_Atual = 2023;
+  let idade = ano_Atual - ano_Nascimento;
 
-  // 2 Escreva uma função que lê dois nº inteiros diferentes e mostre uma mensagem indicando qual é o maior.
+  if (!isNaN(ano_Nascimento)) {
+    if (idade >= 16) {
+      resultadoElement.textContent = 'A pessoa tem ' + idade + ' anos de idade e pode votar';
+    } else {
+      resultadoElement.textContent = 'A pessoa tem ' + idade + ' anos de idade e não pode votar';
+    }
+  } else {
+    resultadoElement.textContent = 'Digite números válidos.';
+  }
+}
 
+//resolucao da 4° pergunta
 
-  // function numeroMaior(idn1, idn2, idresult2) {
-
-  //   let n1 = parseInt(document.getElementById(idn1).value);
-  //   let n2 = parseInt(document.getElementById(idn2).value);
-  //   let result2 = document.getElementById(idresult2);
-    // }
-
-  // 3 Escreva uma função que lê o ano de nascimento de uma pessoa. Mostre uma mensagem que diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
-
-  
-  // function votaOuNaoVota(numero1) {
-  //   let ano_Nascimento = parseInt(document.getElementById(numero1).value);
-  //   let ano_Atual = 2023;
-  //   let idade = ano_Atual - ano_Nascimento;
-  //   idade >= 16 ? console.log('A pessoa tem ' + idade + ' anos de idade e pode votar') : console.log('A pessoa tem ' + idade + ' anos de idade e não pode votar');
-  // }
+function reajusteSalarial(idInput1, idInput2, idResultado) {
+  const salario_Atual = parseInt(document.getElementById(idInput1).value);
+  const porcentagem = parseInt(document.getElementById(idInput2).value);
+  const resultadoElement = document.getElementById(idResultado);
+  if (!isNaN(salario_Atual) && !isNaN(porcentagem)) {
+    if (porcentagem > 0) {
+      resultadoElement.textContent = 'O reajuste é de R$'+(salario_Atual+ (porcentagem/100)*salario_Atual);
+    }else {
+      resultadoElement.textContent = 'A porcentagem esta incorreta.';
+    }
+  }
+  else {
+    resultadoElement.textContent = 'Digite números válidos.';
+  }
+}
